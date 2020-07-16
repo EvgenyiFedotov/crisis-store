91466ac3e2db4bfb2114bf59ffee5b5bb3cb156e
e.fedotov <fedotov1290@gmail.com>
2020-07-16 19:19:12 +0300

    deploy

---
+++ precache-manifest.adcb7533ce1ed53d6a251125517b75e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0051f8687e445394fe3ef7244e111e2c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "819b7e775cc2e9f10357",
+    "url": "./static/css/main.9dac40e2.chunk.css"
+  },
+  {
+    "revision": "146eba1ba82d88049505",
+    "url": "./static/js/2.4bc6891e.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "./static/js/2.4bc6891e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819b7e775cc2e9f10357",
+    "url": "./static/js/main.144ceb04.chunk.js"
+  },
+  {
+    "revision": "3072a3dc2bf27c30e550",
+    "url": "./static/js/runtime-main.873f9c61.js"
+  },
+  {
+    "revision": "385e780011dfcd6f2d9d96804143e799",
+    "url": "./static/media/main-bg-2.385e7800.jpg"
+  },
+  {
+    "revision": "1f833ff58c0677d40599a639b09d7953",
+    "url": "./static/media/main-bg-3.1f833ff5.jpg"
+  },
+  {
+    "revision": "49f41097bd5ac42275e3396a711d6a81",
+    "url": "./static/media/main-bg-4.49f41097.jpg"
+  }
+]);
